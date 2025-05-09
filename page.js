2340837b2433e1357ab4ffe8ed61e1c1d763e587
邵子欣 <2404363710@qq.com>
2025-05-09 13:24:05 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+import  Navbar from "../components/Navbar";
+export default function Hello(){
+  return (
+    <div>
+       <Navbar />
+      <h1 className="text-teal-300 bg-sky-50 text-8xl text-center italic font-serif">Hello</h1>
+      </div>
+  )
+}
